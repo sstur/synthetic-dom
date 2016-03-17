dfc678e1598f550be45f32a60cfe5af4c86afe7a
Simon Sturmer <sstur@me.com>
2016-03-17 20:34:20 +0700

    be less verbose; better tests

--- src/SyntheticDOM.js
+++ src/SyntheticDOM.js
@@ -1,11 +1,16 @@
 /* @flow */
 
+type Attr = [string, string];
+type MapList = {[key: string]: boolean};
+
 export const NODE_TYPE_ELEMENT = 1;
 export const NODE_TYPE_TEXT = 3;
 export const NODE_TYPE_FRAGMENT = 11;
-export const SELF_CLOSING: Array<Attr> = [];
-
-type Attr = [string, string];
+export const SELF_CLOSING: MapList = {
+  area: true, base: true, br: true, col: true, embed: true, hr: true, img: true,
+  input: true, keygen: true, link: true, meta: true, param: true, source: true,
+  track: true, wbr: true,
+};
 
 export class Node {
   nodeType: number;
@@ -28,17 +33,15 @@ export class TextNode extends Node {
 
 export class ElementNode extends Node {
   childNodes: Array<Node>;
-  attributes: Array<Attr>;
-  attributesMap: Map<string, string>;
+  attributes: Map<string, string>;
   isSelfClosing: boolean;
 
-  constructor(name: string, attributes: Array<Attr>, childNodes: Array<Node>) {
+  constructor(name: string, attributes: ?Array<Attr>, childNodes: ?Array<Node>) {
     super(...arguments);
-    let isSelfClosing = (childNodes === SELF_CLOSING);
+    let isSelfClosing = (SELF_CLOSING[name] === true);
     this.nodeType = NODE_TYPE_ELEMENT;
     this.nodeName = name;
-    this.attributes = attributes || [];
-    this.attributesMap = new Map(attributes);
+    this.attributes = (attributes == null) ? new Map() : new Map(attributes);
     this.childNodes = [];
     this.isSelfClosing = isSelfClosing;
     if (!isSelfClosing && childNodes) {
@@ -57,24 +60,20 @@ export class ElementNode extends Node {
   }
 
   getAttribute(name: string): ?string {
-    return this.attributesMap.get(name);
+    return this.attributes.get(name);
   }
 
-  toString(isXHTML: boolean): string {
-    let attributes = this.attributes.map(
-      (attr) => attr[0] + (attr[1] ? '=' + escapeAttr(attr[1]) : '')
-    ).join(' ');
-    if (attributes.length) {
-      attributes = ' ' + attributes;
+  toString(isXHTML: ?boolean): string {
+    let attributes = [];
+    for (let [name, value] of this.attributes.entries()) {
+      attributes.push(name + (value ? '="' + escapeAttr(value) + '"' : ''));
     }
+    let attrString = attributes.length ? ' ' + attributes.join(' ') : '';
     if (this.isSelfClosing) {
-      return '<' + this.nodeName + attributes + (isXHTML ? '/>' : '>');
-    }
-    if (this.childNodes != null) {
-
+      return '<' + this.nodeName + attrString + (isXHTML ? '/>' : '>');
     }
     let childNodes = this.childNodes.map((node) => node.toString(isXHTML)).join('');
-    return '<' + this.nodeName + attributes + '>' + childNodes + '</' + this.nodeName + '>';
+    return '<' + this.nodeName + attrString + '>' + childNodes + '</' + this.nodeName + '>';
   }
 }
 
--- src/__tests__/SyntheticDOM-test.js
+++ src/__tests__/SyntheticDOM-test.js
@@ -18,12 +18,51 @@ describe('Text Nodes', () => {
 });
 
 describe('Elements', () => {
-  let emptyAttributes = [];
-  let element = new ElementNode('div', emptyAttributes, []);
-  it('should get created properly', () => {
+  it('should accept empty attributes and empty children', () => {
+    let element = new ElementNode('div', [], []);
     expect(element.nodeType).toBe(NODE_TYPE_ELEMENT);
     expect(element.nodeName).toBe('div');
-    expect(element.attributes).toBe(emptyAttributes);
+    expect(element.attributes).toBeA(Map);
+    expect(element.attributes.size).toBe(0);
     expect(element.childNodes).toEqual([]);
+  });
+
+  it('should accept null attributes and some children', () => {
+    let textNode = new TextNode('Hello World');
+    let element = new ElementNode('div', null, [textNode]);
+    expect(element.attributes).toBeA(Map);
+    expect(element.attributes.size).toBe(0);
+    expect(element.toString()).toBe('<div>Hello World</div>');
+  });
+
+  it('should accept null attributes and null children', () => {
+    let element = new ElementNode('div');
+    expect(element.attributes).toBeA(Map);
+    expect(element.attributes.size).toBe(0);
+    expect(element.toString()).toBe('<div></div>');
+  });
+
+  it('should ignore children for self-closing (void) tags', () => {
+    let p = new ElementNode('p');
+    let element = new ElementNode('hr', null, [p]);
+    expect(element.childNodes.length).toBe(0);
+    expect(element.toString()).toBe('<hr>');
+  });
+
+  it('should stringify correctly', () => {
+    let br = new ElementNode('br');
+    let p = new ElementNode('p', null, [br, br]);
+    let attrs = [['className', 'foo']];
+    let element = new ElementNode('div', attrs, [p]);
+    expect(element.childNodes.length).toBe(1);
+    let firstChild = element.childNodes[0];
+    // Weird branching to make Flow happy.
+    if (firstChild instanceof ElementNode) {
+      expect(firstChild.childNodes.length).toBe(2);
+    } else {
+      expect(firstChild).toBeAn(ElementNode);
+    }
+    expect(element.toString()).toBe('<div className="foo"><p><br><br></p></div>');
+    expect(element.toString(true)).toBe('<div className="foo"><p><br/><br/></p></div>');
   });
 });
